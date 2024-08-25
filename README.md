cd9648ea85a0d546c1f1a7f8c54e7a16dd982bcd
thinkgos <thinkgo@aliyun.com>
2024-08-25 16:57:24 +0800

    fix: add tools

---
+++ README.md
@@ -0,0 +1,14 @@
+# esp-idf-iot
+
+## wokwi
+
+[wokwi-rust](https://wokwi.com/rust)
+
+## References
+
+- [The Rust on ESP Book](https://docs.esp-rs.org/book/)
+- [Embedded Rust on Espressif(std)](https://docs.esp-rs.org/std-training/)
+- [Embedded Rust on Espressif(no-std)](https://docs.esp-rs.org/no_std-training/)
+- [awesome-esp-rust](https://github.com/esp-rs/awesome-esp-rust)
+- [The Embedded Rust Book](https://docs.rust-embedded.org/book/index.html)
+- [The Embedonomicon](https://docs.rust-embedded.org/embedonomicon/)
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-fn main() {
+use esp_idf_svc::hal::delay::FreeRtos;
+use esp_idf_svc::hal::gpio::PinDriver;
+use esp_idf_svc::hal::peripherals::Peripherals;
+
+fn main() -> anyhow::Result<()> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
@@ -7,4 +11,17 @@ fn main() {
     esp_idf_svc::log::EspLogger::initialize_default();
 
     log::info!("Hello, world!");
+
+    let peripherals = Peripherals::take().unwrap();
+
+    let mut led = PinDriver::output(peripherals.pins.gpio4)?;
+
+    loop {
+        led.set_high()?;
+        // we are sleeping here to make sure the watchdog isn't triggered
+        FreeRtos::delay_ms(1000);
+
+        led.set_low()?;
+        FreeRtos::delay_ms(1000);
+    }
 }
